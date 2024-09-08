a6017de995aa400a8431f1e444c1283793d7f4c4
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-09-08 22:21:48 +0300

    Update chatUncensored.js

---
+++ chatUncensored.js
@@ -0,0 +1,8 @@
+
+#!name= chat Uncensored
+#!desc= Active Premium chat
+
+[Script]
+Nicegram = type=http-response,pattern= ^https?:\/\/api\.adapty\.io\/api\/v1\/sdk\/in-apps\/apple\/receipt\/validate\/,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/sultan1sa/AllinOneRewrite-/main/ChatUncensored.js,script-update-interval=0
+[MITM]
+hostname =  api.adapty.io
