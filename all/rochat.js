f59cde91ebfb3f4bd53ef19a10e0642e525e598e
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-10-04 05:07:25 +0300

    Update rochat.js

---
+++ all/rochat.js
@@ -0,0 +1,35 @@
+/*
+rochat
+
+[rewrite_local]
+https://api.rochathub.com/inteco.user.v1.UserRightsService/GetUserRights url script-response-body https://raw.githubusercontent.com/sultan1sa/Rewrite/refs/heads/main/all/rochat.js
+
+[mitm]
+hostname = api.rochathub.com
+
+*/
+
+var objc = JSON.parse($response.body);
+    objc = {
+  "rights" : {
+    "generalbotLeftTimes" : 29,
+    "premiumbotLeftTimes" : 595,
+    "gptLeftTimes" : 296,
+    "premiumbotTotalTimes" : 600,
+    "gptDisplayName" : "GPT-4",
+    "gptTotalTimes" : 300,
+    "haveStripeBill" : true,
+    "generabotDisplayName" : "通用Bot",
+    "premiumbotDisplayName" : "专业Bot",
+    "generalbotTotalTimes" : 30,
+    "memberShipRights" : {
+      "term" : "YEARLY",
+      "starttime" : "1706442853",
+      "expiredtime" : "2706702060"
+    },
+    "isGeneralbotUnlimited" : true
+  }
+}
+
+
+$done({body : JSON.stringify(objc)});
