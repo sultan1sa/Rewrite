c1597ee392eaacbfad44f43c5a078ac65eb2ef99
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-11-12 15:02:56 +0300

    Update Xsearch.js

---
+++ Xsearch.js
@@ -0,0 +1,72 @@
+#name=MultiSearch
+
+#!desc=多重搜索
+
+#!homepage=https://whatshub.top
+
+#!author=deezertidal
+
+#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/multisearch.png
+
+
+
+# 自用 请根据需求自行修改
+
+# 默认Google搜索
+
+# wk+空格+关键词(维基百科)
+
+# bd+空格+关键词(百度)
+
+# gh+空格+关键词(GitHub)
+
+# gi+空格+关键词(Google 图片)
+
+# yd+空格+关键词(有道词典)
+
+# trc+空格+关键词(Google 译至中)
+
+# tre+空格+关键词(Google 译至英)
+
+# db+空格+关键词(豆瓣)
+
+# zh+空格+关键词(知乎)
+
+# wb+空格+关键词(微博)
+
+# ddg+空格+关键词(DuckDuckGo)
+
+# bi+空格+关键词(Bing)
+
+
+
+[URL Rewrite]
+
+^https:\/\/www\.google\.com\/search\?q=gpt(\+|%20)([^&]+).+ https://chatgpt.com/?q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=gen(\+|%20)([^&]+).+ https://www.genspark.ai/search?q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=gh(\+|%20)([^&]+).+ https://github.com/search?type=code&q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=gi(\+|%20)([^&]+).+ https://isou.chat/search?q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=yd(\+|%20)([^&]+).+ ‎‏https://yandex.com/images/touch/search?lr=21467&text=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=exa(\+|%20)([^&]+).+ https://exa.ai/search?q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=tre(\+|%20)([^&]+).+ https://searx.be/search?q=site$2 302
+
+^https:\/\/www\.google\.com\/search\?q=db(\+|%20)([^&]+).+ https://dsearch.com/search?q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=zh(\+|%20)([^&]+).+ https://yandex.com/images/touch/search?lr=21467&text=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=ru(\+|%20)([^&]+).+ https://ya.ru/search/?text=$2 302
+
+^https:\/\/www\.google\.com\/search\?git=cod(\+|%20)([^&]+).+ https://gist.github.com/search?q=$2 302
+
+^https:\/\/www\.google\.com\/search\?q=git(\+|%20)([^&]+).+ https://gist.github.com/search?q=$2 302
+
+
+[MITM]
+
+hostname = %APPEND% www.google.com
